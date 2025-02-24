7004b7d2e2a60c1a05913e25515d49a7b4e4a1b2
Luke Lauter <llauter@cannellamedia.com>
2025-02-24 12:18:21 -0600

    Included nuget pack cmd

---
+++ pack-MudBlazor.cmd
@@ -0,0 +1,2 @@
+dotnet pack .\src\MudBlazor\MudBlazor.csproj --include-symbols -p:PackageVersion=8.3.0-cm-x01 --output ..\nuget
+dotnet nuget push --source "Cannella" --api-key az ..\nuget\MudBlazor.8.3.0-cm-x01.nupkg
