adf161e6282428a29c8436087720a08c7aedafc6
Luke Lauter <llauter@cannellamedia.com>
2025-04-09 12:34:11 -0500

    Updated Dialog Overlay to not lock scrolling, as it's currently not un-applying the class on close.

--- pack-MudBlazor.cmd
+++ pack-MudBlazor.cmd
@@ -1,2 +1,2 @@
-dotnet pack .\src\MudBlazor\MudBlazor.csproj --include-symbols -p:PackageVersion=8.3.0-cm-x04 --output ..\nuget
-dotnet nuget push --source "Cannella" --api-key az ..\nuget\MudBlazor.8.3.0-cm-x04.nupkg
+dotnet pack .\src\MudBlazor\MudBlazor.csproj --include-symbols -p:PackageVersion=8.5.1-cm-x02 --output ..\nuget
+dotnet nuget push --source "Cannella" --api-key az ..\nuget\MudBlazor.8.5.1-cm-x02.nupkg
