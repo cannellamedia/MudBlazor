408e060627bb2efc9ef887617a7400fcbcd53537
Luke Lauter <llauter@cannellamedia.com>
2025-05-19 11:00:05 -0400

    Updated release pack cmd.

--- pack-MudBlazor.cmd
+++ pack-MudBlazor.cmd
@@ -1,2 +1,2 @@
-dotnet pack .\src\MudBlazor\MudBlazor.csproj --include-symbols -p:PackageVersion=8.6.0-cm-x02 --output ..\nuget
-dotnet nuget push --source "Cannella" --api-key az ..\nuget\MudBlazor.8.6.0-cm-x02.nupkg
+dotnet pack .\src\MudBlazor\MudBlazor.csproj --include-symbols -p:PackageVersion=8.6.0-cm-x03 --output ..\nuget
+dotnet nuget push --source "Cannella" --api-key az ..\nuget\MudBlazor.8.6.0-cm-x03.nupkg
